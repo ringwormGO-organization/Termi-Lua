3d89ae64767bce56786d1d1a6b3c1cc04b611af2
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2022-03-08 20:02:09 +0100

    Version 0.0.4

--- src/Main.lua
+++ src/Main.lua
@@ -1,5 +1,5 @@
 -- LUA 5.1 MINIMUM
--- TERMI-LUA VERSION 0.0.3
+-- TERMI-LUA VERSION 0.0.4
 --------------------------
 
 print("\n")
@@ -10,20 +10,45 @@ print("    888      888ooo8     888    888 888 888 888   888 ")
 print("    888      888    oo   888        888 888 888   888  ")
 print("   o888o    o888ooo8888 o888o      o888o888o888o o888o ")
 print("-------------------------------------------------------")
-print("	TYPE 'Help' TO SEE ALL COMMANDS")
-print("		(C) 2022 ringwormGO                   ")
+print("	 Type '!help', '!credits', '!exit'")
 print("-------------------------------------------------------")
 
 repeat
-	io.write("Termi> ")
 
+	
+	io.write("Termi> ")
+	
 	input = io.read()
 
-	if input == "Help" then
-		print("Help -- shows list of commands")
-		print("Opencalc -- opens a calculator")
-		print("OpenGeocalc -- opens a geometric calculator")
-		print("Exit -- exits Termi")
+	ver = "0.0.4"
+
+	if input == "!help" then
+		print("COMMANDS THAT START WITH ! :")
+		print("	!help -- shows list of commands")
+		print("	!credits -- prints out credits")
+		print("	!ver -- prints out version of termi")
+		print("	!exit -- exits Termi")
+		print("Calculator commands :")
+		print("	Opencalc -- opens a calculator")
+		print("	OpenGeocalc -- opens a geometric calculator")
+		print("Filesys commands : // WORK IN PROGRESS")
+		print("	filesys -- opens filesys interface")
+		print("	createf -- creates a file")
+		print("	openf -- opens a file")
+		print("	cd -- cds into a direcotry")
+		print("	rm -- removes a file/directory")
+		print("	mkdir -- creates a directory")
+		print(" list -- lists files in a directory\n")
+		print("|-------------------------------------------|")
+
+	elseif input == "!credits" then
+		print("* @author(s): StjepanBM1, Andrej123456789")
+		print("* PROJECT: Termi (Lua version)")
+		print("* Licnese: BSD 3-Clause License")
+		print("* DESCRIPTION: Main file for Termi (Lua version)")
+
+	elseif input == "!ver" then
+		print(ver)
 
 	elseif input == "Opencalc" then
 
@@ -149,4 +174,4 @@ repeat
 
 end
 
-until tostring(input) == "Exit"
+until tostring(input) == "!exit"
