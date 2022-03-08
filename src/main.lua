f4dc5b80def0e88f3ddd321e32b89626e55ffdef
StjepanBM1 <94230991+StjepanBM1@users.noreply.github.com>
2022-03-08 21:20:42 +0100

    Version 0.0.5

---
+++ src/main.lua
@@ -0,0 +1,56 @@
+-- LUA 5.1 MINIMUM
+-- TERMI-LUA VERSION 0.0.5
+--------------------------
+
+print("\n")
+print("ooooooooooo                              ")
+print("    888      ooooooooooo                          o88   ")
+print("    888      888    88  oo oooooo  oo ooo oooo   oooo  ")
+print("    888      888ooo8     888    888 888 888 888   888 ")
+print("    888      888    oo   888        888 888 888   888  ")
+print("   o888o    o888ooo8888 o888o      o888o888o888o o888o ")
+print("-------------------------------------------------------")
+print("	 Type '!help', '!credits', '!exit'")
+print("-------------------------------------------------------")
+
+repeat
+
+	
+	io.write("Termi> ")
+	
+	input = io.read()
+
+	ver = "0.0.5"
+
+	if input == "!help" then
+		dofile("functions/help.lua")
+
+	elseif input == "!credits" then
+		dofile("functions/credits.lua")
+
+	elseif input == "!ver" then
+		print(ver)
+
+	elseif input == "createf" then
+		dofile("functions/createf.lua")
+
+	elseif input == "openf" then
+
+	elseif input == "mkdir" then
+		dofile("functions/mkdir.lua")
+
+	elseif input == "rm" then
+		dofile("functions/rm.lua")
+
+	elseif input == "Opencalc" then
+
+		dofile("functions/Calc.lua")
+
+	elseif input == "OpenGeocalc" then
+		dofile("functions/Geocalc.lua")
+
+	else
+
+	end
+
+until tostring(input) == "!exit"
