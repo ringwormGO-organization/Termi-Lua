8fff7d3a54e53ff78b24cbc89312de4d4935b751
StjepanBM1 <stjepan.bilicmatsic546@gmail.com>
2022-06-28 08:57:35 +0200

    Echo command added

---
+++ src/functions/echo.lua
@@ -0,0 +1,20 @@
+
+---
+--- @author Stjepan Bilić Matišić
+--- @PROJECT: Termi-Lua
+--- @LICENSE: ringwormGO General License 1.0 | (RGL) 2022
+--- @DESCRIPTION: echo command file
+---
+
+local echo = {}
+
+local util = require("functions.utils.utils")
+
+function echo.Run(arg)
+    for i = 2, util.Length(arg), 1 do
+        io.write(arg[i], " ")
+    end
+
+end
+
+return echo
